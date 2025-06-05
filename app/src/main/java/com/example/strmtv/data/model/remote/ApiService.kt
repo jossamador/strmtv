382799a1bb707674a440a9b4f123448762315275
jossamador <jos-amador@hotmail.com>
2025-06-05 16:13:37 -0600

    Commit 4: Se agregó una interfaz ApiService con endpoint para obtener los items desde la API

--- app/src/main/java/com/example/strmtv/data/model/remote/ApiService.kt
+++ app/src/main/java/com/example/strmtv/data/model/remote/ApiService.kt
@@ -1,2 +1,9 @@
-package com.example.strmtv.data.model.remote
+package com.example.strmtv.data.remote
 
+import com.example.strmtv.data.model.Item
+import retrofit2.http.GET
+
+interface ApiService {
+    @GET("/api/items")
+    suspend fun getItems(): List<Item>
+}
