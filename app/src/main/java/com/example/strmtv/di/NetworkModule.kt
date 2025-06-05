2fa3ba34d47f9b4b8930081f3d314101aa94e484
jossamador <jos-amador@hotmail.com>
2025-06-05 16:41:06 -0600

    Commit 6: Se agregó el archivo NetowkrModule, para inyectar retrofit, api service y repositorio con hilt

--- app/src/main/java/com/example/strmtv/di/NetworkModule.kt
+++ app/src/main/java/com/example/strmtv/di/NetworkModule.kt
@@ -1,2 +1,37 @@
 package com.example.strmtv.di
 
+import com.example.strmtv.data.remote.ApiService
+import com.example.strmtv.data.repository.ItemRepositoryImpl
+import com.example.strmtv.domain.repository.ItemRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    private const val BASE_URL = "http://10.0.2.2:3000" // Para emulador de Android Studio
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): ApiService =
+        retrofit.create(ApiService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideItemRepository(apiService: ApiService): ItemRepository =
+        ItemRepositoryImpl(apiService)
+}
