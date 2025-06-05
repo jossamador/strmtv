a7083a052450d27000feee214adde154926b82b8
jossamador <jos-amador@hotmail.com>
2025-06-05 16:20:42 -0600

    Commit 5: Se agregó archivo ItemRepositoryImpl  del consumo de la API con retrofit

--- app/src/main/java/com/example/strmtv/data/model/repository/ItemRepositoryImpl.kt
+++ app/src/main/java/com/example/strmtv/data/model/repository/ItemRepositoryImpl.kt
@@ -1,4 +1,14 @@
-package com.example.strmtv.data.model.repository
+package com.example.strmtv.data.repository
 
-class ItemRepositoryImpl {
+import com.example.strmtv.data.remote.ApiService
+import com.example.strmtv.data.model.Item
+import com.example.strmtv.domain.repository.ItemRepository
+import javax.inject.Inject
+
+class ItemRepositoryImpl @Inject constructor(
+    private val apiService: ApiService
+) : ItemRepository {
+    override suspend fun getItems(): List<Item> {
+        return apiService.getItems()
+    }
 }
