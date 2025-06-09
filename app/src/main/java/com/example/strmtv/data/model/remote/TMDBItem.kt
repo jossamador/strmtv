dda2e9b6c492257c8f76afa2a84db2aa72cfa2e0
jossamador <jos-amador@hotmail.com>
2025-06-09 15:40:12 -0600

    Commit 22: Se agregó el archivo TMDBItem.kt para recibir respuesta de items

---
+++ app/src/main/java/com/example/strmtv/data/model/remote/TMDBItem.kt
@@ -0,0 +1,23 @@
+package com.example.strmtv.data.model.remote
+
+import com.google.gson.annotations.SerializedName
+
+data class TMDBItem(
+    @SerializedName("title")
+    val title: String? = null,
+
+    @SerializedName("name")
+    val name: String? = null,
+
+    @SerializedName("poster")
+    val posterPath: String? = null,
+
+    @SerializedName("media_type")
+    val mediaType: String? = null,
+
+    @SerializedName("overview")
+    val overview: String? = null,
+
+    @SerializedName("release_date")
+    val releaseDate: String? = null
+)
