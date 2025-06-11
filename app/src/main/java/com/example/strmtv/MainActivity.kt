0e1d21c979b4c475d0655ea31701b55d24fd7b06
jossamador <jos-amador@hotmail.com>
2025-06-11 14:55:53 -0600

    Commit 28: Se configuró el MainActivity para utilizar la NavGraph en la navegación de la app

--- app/src/main/java/com/example/strmtv/MainActivity.kt
+++ app/src/main/java/com/example/strmtv/MainActivity.kt
@@ -3,10 +3,16 @@ package com.example.strmtv
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.rememberNavController
-import com.example.strmtv.presentation.navigation.AppNavGraph
+import com.example.strmtv.presentation.home.navigation.NavGraph
 import com.example.strmtv.ui.theme.StrmtvTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,9 +24,21 @@ class MainActivity : ComponentActivity() {
             StrmtvTheme {
                 Surface(color = MaterialTheme.colorScheme.background) {
                     val navController = rememberNavController()
-                    AppNavGraph(navController = navController)
+                    NavGraph(navController = navController)
                 }
             }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    StrmtvTheme {
+        Text(
+            text = "Hello Strmtv!",
+            fontSize = 20.sp,
+            color = Color.Black
+        )
     }
 }
