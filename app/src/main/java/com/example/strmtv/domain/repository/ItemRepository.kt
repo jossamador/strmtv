13df53a2419cb116a1c8dffedf9b6c497a55e788
jossamador <jos-amador@hotmail.com>
2025-06-11 15:21:17 -0600

    Commit 34: Se actualizó la interfaz ItemRepository para incluir búsqueda local y remota

--- app/src/main/java/com/example/strmtv/domain/repository/ItemRepository.kt
+++ app/src/main/java/com/example/strmtv/domain/repository/ItemRepository.kt
@@ -1,7 +1,13 @@
 package com.example.strmtv.domain.repository
 
 import com.example.strmtv.data.model.Item
+import com.example.strmtv.data.model.remote.TMDBItem
 
 interface ItemRepository {
+
     suspend fun getItems(): List<Item>
+
+    suspend fun searchLocal(query: String): List<Item>
+
+    suspend fun searchTMDB(query: String): List<TMDBItem>
 }
