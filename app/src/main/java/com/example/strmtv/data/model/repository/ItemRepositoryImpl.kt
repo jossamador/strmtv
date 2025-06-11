2f0f591be7360a804ec99c21e36c6e8209eb16fc
jossamador <jos-amador@hotmail.com>
2025-06-11 15:12:54 -0600

    Commit 32: Se integró el consumo de la API de TMDB y Local en ItemRepositoryImpl

--- app/src/main/java/com/example/strmtv/data/model/repository/ItemRepositoryImpl.kt
+++ app/src/main/java/com/example/strmtv/data/model/repository/ItemRepositoryImpl.kt
@@ -1,14 +1,47 @@
-package com.example.strmtv.data.repository
+package com.example.strmtv.data.model.repository
 
-import com.example.strmtv.data.remote.ApiService
 import com.example.strmtv.data.model.Item
+import com.example.strmtv.data.model.remote.LocalApiService
+import com.example.strmtv.data.model.remote.TMDBApiService
+import com.example.strmtv.data.model.remote.TMDBItem
 import com.example.strmtv.domain.repository.ItemRepository
 import javax.inject.Inject
 
 class ItemRepositoryImpl @Inject constructor(
-    private val apiService: ApiService
+    private val tmdbApiService: TMDBApiService,
+    private val localApiService: LocalApiService
 ) : ItemRepository {
+
+    // Buscar en Local
+    override suspend fun searchLocal(query: String): List<Item> {
+        return try {
+            localApiService.searchLocal(query)
+        } catch (e: Exception) {
+            emptyList()
+        }
+    }
+
+    // Buscar en TMDB
+    override suspend fun searchTMDB(query: String): List<TMDBItem> {
+        return try {
+            val response = tmdbApiService.searchMulti(query, TMDB_API_KEY)
+            response.results
+        } catch (e: Exception) {
+            println("Error al buscar en TMDB: ${e.message}")
+            emptyList()
+        }
+    }
+
+    // Implementación de getItems()
     override suspend fun getItems(): List<Item> {
-        return apiService.getItems()
+        return try {
+            localApiService.getItems()
+        } catch (e: Exception) {
+            emptyList()
+        }
+    }
+
+    companion object {
+        const val TMDB_API_KEY = "1087b47ed60a7c149fc4101df16584ed"
     }
 }
