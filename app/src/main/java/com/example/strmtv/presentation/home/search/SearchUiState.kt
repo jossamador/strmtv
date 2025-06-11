5080b4b6d50bff3c39035c87ad49da0ae1c95ad0
jossamador <jos-amador@hotmail.com>
2025-06-11 15:47:47 -0600

    Commit 39: Se actualizó el estado de la UI (SearchUiState) para soportar resultados de búsqueda TMDB y Local

--- app/src/main/java/com/example/strmtv/presentation/home/search/SearchUiState.kt
+++ app/src/main/java/com/example/strmtv/presentation/home/search/SearchUiState.kt
@@ -1,10 +1,12 @@
 package com.example.strmtv.presentation.home.search
 
+import com.example.strmtv.data.model.Item
 import com.example.strmtv.data.model.remote.TMDBItem
 
 sealed class SearchUiState {
     object Idle : SearchUiState()
     object Loading : SearchUiState()
-    data class Success(val results: List<TMDBItem>) : SearchUiState()
+    data class SuccessLocal(val results: List<Item>) : SearchUiState()
+    data class SuccessTMDB(val results: List<TMDBItem>) : SearchUiState()
     data class Error(val message: String) : SearchUiState()
 }
