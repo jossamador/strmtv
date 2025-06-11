ffbffe30cfda4cd3a0aea5f0ac6d077b8c5dff0a
jossamador <jos-amador@hotmail.com>
2025-06-11 15:18:40 -0600

    Commit 33: Se configuró NetworkModule para proporcionar Retrofit y los servicios de red

--- app/src/main/java/com/example/strmtv/di/NetworkModule.kt
+++ app/src/main/java/com/example/strmtv/di/NetworkModule.kt
@@ -1,50 +1,36 @@
 package com.example.strmtv.di
 
-import com.example.strmtv.data.remote.ApiService
-import com.example.strmtv.data.repository.ItemRepositoryImpl
-import com.example.strmtv.domain.repository.ItemRepository
+import com.example.strmtv.data.model.remote.LocalApiService
+import com.example.strmtv.data.model.remote.TMDBApiService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
-    private const val BASE_URL = "http://10.0.2.2:3000" // Cambia a 8080 si hiciste ese cambio
-
     @Provides
     @Singleton
-    fun provideOkHttpClient(): OkHttpClient {
-        return OkHttpClient.Builder()
-            .connectTimeout(15, TimeUnit.SECONDS)
-            .readTimeout(15, TimeUnit.SECONDS)
-            .writeTimeout(15, TimeUnit.SECONDS)
+    fun provideTMDBApiService(): TMDBApiService {
+        return Retrofit.Builder()
+            .baseUrl("https://api.themoviedb.org/3/")
+            .addConverterFactory(GsonConverterFactory.create())
             .build()
+            .create(TMDBApiService::class.java)
     }
 
     @Provides
     @Singleton
-    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
-        Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .client(okHttpClient)
+    fun provideLocalApiService(): LocalApiService {
+        return Retrofit.Builder()
+            .baseUrl("http://10.0.2.2:3000/") // tu API local
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-
-    @Provides
-    @Singleton
-    fun provideApiService(retrofit: Retrofit): ApiService =
-        retrofit.create(ApiService::class.java)
-
-    @Provides
-    @Singleton
-    fun provideItemRepository(apiService: ApiService): ItemRepository =
-        ItemRepositoryImpl(apiService)
+            .create(LocalApiService::class.java)
+    }
 }
