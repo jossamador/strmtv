3f79791090f9f76b4fe74fede062f964edb75969
jossamador <jos-amador@hotmail.com>
2025-06-15 22:11:57 -0600

    Commit 53: Se agregó una dependencia nueva para complementar la pantalla de busqueda

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,5 +85,6 @@ dependencies {
     // Navigation Compose
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
-    implementation("androidx.compose.material:material-icons-extended:<version>")
+    implementation("androidx.compose.material:material:1.6.7")
+    implementation("androidx.compose.material:material-icons-extended:1.6.7")
 }
